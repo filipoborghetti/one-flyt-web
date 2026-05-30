68e413c5f4374bd8b74f0367b44eb5aeecff98e6
Filipo Borghetti <renato.filipo@gmail.com>
2026-05-30 19:39:51 -0300

    agregar sitemap y robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://one-flyt-web.vercel.app",
+  integrations: [sitemap()],
+});
